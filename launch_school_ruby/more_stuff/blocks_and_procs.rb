eb7821183374b10627242f068ab3558070dbd8ce
Noah Kawasaki <ntkawasaki@gmail.com>
2018-11-25 20:17:02 -0800

    rubocop

--- launch_school_ruby/more_stuff/blocks_and_procs.rb
+++ launch_school_ruby/more_stuff/blocks_and_procs.rb
@@ -1,6 +1,7 @@
 ## block
 def take_block(number, &block)
-  block.call(number)
+  # block.call(number)
+  yield number
 end
 
 
@@ -12,22 +13,22 @@ end
 
 
 ## procs
-talk = Proc.new do |name|
+talk = proc do |name|
   puts "I am talking to #{name}"
 end
 
-talk.call("Bob")
+talk.call('Bob')
 
 
 # modify take_block
 def take_proc(proc)
-  [1, 2, 3, 4, 5].each do |num| 
+  [1, 2, 3, 4, 5].each do |num|
     proc.call num
   end
 end
 
-proc = Proc.new do |number|
-  puts "#{number}. Proc being called in the method!"
+my_proc = proc do |x|
+  puts "#{x}. Proc being called in the method!"
 end
 
-take_proc(proc)
+take_proc(my_proc)
