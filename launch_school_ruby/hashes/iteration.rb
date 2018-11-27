cbafa9d6fa4f0d8c92247833c2b7bf9e4532e3d5
Noah Kawasaki <noahkawasaki@Noahs-Air.hsd1.ca.comcast.net>
2018-11-27 06:13:37 -0800

    reorg and finished more stuff

--- launch_school_ruby/hashes/iteration.rb
+++
@@ -1,5 +0,0 @@
-person = {name: 'bob', height: '6 ft', weight: '160 lbs', hair: 'brown'}
-
-p person
-
-person.each { |k, v| puts "Key: #{k}   Value: #{v}"}
--- launch_school_ruby/variables/age.rb
+++
@@ -1,6 +0,0 @@
-puts "How old are you?"
-age = gets.to_i
-
-for i in 1..4 do
-  puts "In #{i * 10} years you will be #{age + i * 10}"
-end
